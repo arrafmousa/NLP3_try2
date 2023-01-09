5ae67450720d67e5c6e993a9f5a3e0a1670e0d1d
arrafmousa <57962306+arrafmousa@users.noreply.github.com>
2023-01-09 20:05:31 +0200

    removed start of the day evaluation

--- finalModel.py
+++ finalModel.py
@@ -138,18 +138,6 @@ model = DependencyParser(100).to(device) # TODO :  change the embedding dim from
 optim = torch.optim.Adam(model.parameters(), lr=0.01)
 model.load_state_dict(torch.load(r'night_model_after_3_epoch', map_location=torch.device('cpu'))) # TODO : remove this ! we want to train from scartch
 
-accuracies = []
-for idx, sentence in tqdm(enumerate(eval_ds)):
-    loss, T = model(sentence)
-    accuracy = np.sum(T[0][1:] == np.array([int(x) for x in sentence["Token Head"].values])) / T[0].size
-    accuracies.append(accuracy)
-
-total_acc = sum(accuracies) / len(accuracies)
-if total_acc > 0.7:
-    print(f"V-V-V-V-V accuracy \t {total_acc} ***** w7sh")
-else:
-    print(f"*X*X*X*X  accuracy \tonly got {total_acc}")
-
 for epoch in EPOCHS:
     losses = []
     accuracies = []
